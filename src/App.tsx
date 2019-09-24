863500f0f76463f3f76fc65152119d58abf804da
cjacobs <apexearth@gmail.com>
2019-09-24 11:12:20 -0700

    Cheated the first bullet because I created this using create-react-app. Says hello world in a <h1> Created a sub-component which takes in an array and renders it as a list. Added the onClick event for when a user clicks on an <li>.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const array = [
+    'When summer ended',
+    'the leaves of snapdragons withered',
+    'taking their shrill-colored mouths with them.',
+    'They were still, so quiet. They were',
+    'violet where umber now is. She hated',
+    'and she hated to see',
+    'them go. Flowers',
+    '',
+    'born when the weather was good - this',
+    'she thinks of, watching the branch of peaches',
+    'daring their ways above the fence, and further,',
+    'two hummingbirds, hovering, stuck to each other,',
+    'arcing their bodies in grim determination',
+    'to find what is good, what is',
+    'given them to find. These are warriors',
+    '',
+    'distancing themselves from history.',
+    'They find peace',
+    'in the way they contain the wind',
+    'and are gone.',
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <h2>Here is a poem:</h2>
+      <List data={array}/>
     </div>
   );
-}
+};
 
 export default App;
+
+
+interface IList {
+  data: string[];
+}
+
+const List: React.FC<IList> = (props: IList) => {
+  const {data} = props;
+
+  /*  The lambda function in the onClick here is less performant but I am doing so to save time. */
+  return (
+    <ul>
+      {data.map((item) => <li onClick={() => alert(item)}>{item}</li>)}
+    </ul>
+  );
+};
